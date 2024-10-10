26dce6873e6dfaa0981e8ceff3e802d88703974a
Shrestha231 <shresthakumari9006335040@gmail.com>
2024-10-10 14:54:52 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	int p,base,result;
+	printf("\n enter the value of pow");
+	scanf("%d",&p);
+	printf("\n enter the value of base");
+	scanf("%d",&base);
+	result=pow(base,p);
+	printf("power=%d",result);
+	return 0;
+}
+	
+	
