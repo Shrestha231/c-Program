cff5f5c2964d4bc6a13aa48f193c262ec4e075dd
Shrestha231 <shresthakumari9006335040@gmail.com>
2024-12-12 20:43:31 +0530

    Add files via upload

---
+++ complex no. addition.c
@@ -0,0 +1,45 @@
+typedef struct complexNumber {
+    int real;
+    int img;
+} complex;
+
+
+complex add(complex x, complex y)
+{
+    // define a new complex number.
+    complex add;
+
+    // add similar type together
+    add.real = x.real + y.real;
+    add.img = x.img + y.img;
+
+    return (add);
+}
+
+int main()
+{
+
+    // define three complex type numbers
+    complex x, y, sum;
+
+    // first complex number
+    x.real = 4;
+    x.img = 5;
+
+    // second complex number
+    y.real = 7;
+    y.img = 11;
+
+    // printing both complex numbers
+    printf(" x = %d + %di\n", x.real, x.img);
+    printf(" y = %d + %di\n", y.real, y.img);
+
+    // call add(a,b) function and
+    // pass complex numbers a & b
+    // as an parameter.
+    sum = add(x, y);
+
+    printf("\n sum = %d + %di", sum.real, sum.img);
+
+    return 0;
+}
